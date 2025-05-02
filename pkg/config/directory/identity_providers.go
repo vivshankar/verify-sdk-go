f06da1a6708ee9974ed3c37ce00ac5374f92ac0f
Sabuj Mondal <sabuj.mondal@partner.ibm.com>
2025-05-02 11:05:56 +0000

    initial refactoring for identity providers

---
+++ pkg/config/directory/identity_providers.go
@@ -0,0 +1,249 @@
+package directory
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/ibm-verify/verify-sdk-go/internal/openapi"
+
+	contextx "github.com/ibm-verify/verify-sdk-go/pkg/core/context"
+	errorsx "github.com/ibm-verify/verify-sdk-go/pkg/core/errors"
+)
+
+type IdentitysourceClient struct {
+	Client *http.Client
+}
+
+type IdentitySource = openapi.IdentitySourceInstancesData
+type IdentitySourceList = openapi.IdentitySourceIntancesDataList
+
+func NewIdentitySourceClient() *IdentitysourceClient {
+	return &IdentitysourceClient{}
+}
+
+func (c *IdentitysourceClient) CreateIdentitysource(ctx context.Context, identitysource *IdentitySource) (string, error) {
+	vc := contextx.GetVerifyContext(ctx)
+	client := openapi.NewClientWithOptions(ctx, vc.Tenant, c.Client)
+	defaultErr := errorsx.G11NError("unable to create identitysource")
+
+	body, err := json.Marshal(identitysource)
+	if err != nil {
+		vc.Logger.Errorf("Unable to marshal identitysource data; err=%v", err)
+		return "", defaultErr
+	}
+
+	resp, err := client.CreateIdentitySourceV2WithBodyWithResponse(ctx, "application/json", bytes.NewBuffer(body), func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", vc.Token))
+		return nil
+	})
+
+	if err != nil {
+		vc.Logger.Errorf("Unable to create identitysource; err=%v", err)
+		return "", defaultErr
+	}
+
+	if resp.StatusCode() != http.StatusCreated {
+		if err := errorsx.HandleCommonErrors(ctx, resp.HTTPResponse, "unable to create identitysource"); err != nil {
+			vc.Logger.Errorf("unable to create the identitysource; err=%s", err.Error())
+			return "", errorsx.G11NError("unable to create the identitysource; err=%s", err.Error())
+		}
+
+		vc.Logger.Errorf("unable to create the identitysource; code=%d, body=%s", resp.StatusCode(), string(resp.Body))
+		return "", errorsx.G11NError("unable to create the identitysource; code=%d, body=%s", resp.StatusCode(), string(resp.Body))
+	}
+
+	return "Identity provider created successfully", nil
+}
+
+func (c *IdentitysourceClient) GetIdentitysource(ctx context.Context, identitysourceName string) (*IdentitySource, string, error) {
+	vc := contextx.GetVerifyContext(ctx)
+	client := openapi.NewClientWithOptions(ctx, vc.Tenant, c.Client)
+	id, err := c.getIdentitysourceId(ctx, identitysourceName)
+	if err != nil {
+		vc.Logger.Errorf("unable to get the group ID; err=%s", err.Error())
+		return nil, "", err
+	}
+
+	resp, err := client.GetInstanceV2WithResponse(ctx, id, func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer "+vc.Token))
+		return nil
+	})
+	if err != nil {
+		vc.Logger.Errorf("unable to get the IdentitySource; err=%s", err.Error())
+		return nil, "", err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		if err := errorsx.HandleCommonErrors(ctx, resp.HTTPResponse, "unable to get IdentitySource"); err != nil {
+			vc.Logger.Errorf("unable to get the IdentitySource; err=%s", err.Error())
+			return nil, "", err
+		}
+
+		vc.Logger.Errorf("unable to get the IdentitySource; code=%d, body=%s", resp.StatusCode(), string(resp.Body))
+		return nil, "", errorsx.G11NError("unable to get the IdentitySource")
+	}
+
+	IdentitySource := &IdentitySource{}
+	if err = json.Unmarshal(resp.Body, IdentitySource); err != nil {
+		return nil, "", errorsx.G11NError("unable to get the IdentitySource")
+	}
+
+	return IdentitySource, resp.HTTPResponse.Request.URL.String(), nil
+}
+
+func (c *IdentitysourceClient) GetIdentitysources(ctx context.Context, sort string, count string) (*IdentitySourceList, string, error) {
+
+	vc := contextx.GetVerifyContext(ctx)
+	client := openapi.NewClientWithOptions(ctx, vc.Tenant, c.Client)
+	params := &openapi.GetInstancesV2Params{}
+	if len(sort) > 0 {
+		params.Sort = &sort
+	}
+	if len(count) > 0 {
+		params.Count = &count
+	}
+
+	resp, err := client.GetInstancesV2WithResponse(ctx, params, func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", vc.Token))
+		return nil
+	})
+
+	if err != nil {
+		vc.Logger.Errorf("unable to get the Identitysources; err=%s", err.Error())
+		return nil, "", err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		if err := errorsx.HandleCommonErrors(ctx, resp.HTTPResponse, "unable to get Identitysources"); err != nil {
+			vc.Logger.Errorf("unable to get the Identitysources; err=%s", err.Error())
+			return nil, "", err
+		}
+
+		vc.Logger.Errorf("unable to get the Identitysources; code=%d, body=%s", resp.StatusCode(), string(resp.Body))
+		return nil, "", errorsx.G11NError("unable to get the Identitysources")
+	}
+
+	IdentitysourcesResponse := &IdentitySourceList{}
+	if err = json.Unmarshal(resp.Body, &IdentitysourcesResponse); err != nil {
+		vc.Logger.Errorf("unable to get the Identitysources; err=%s, body=%s", err, string(resp.Body))
+		return nil, "", errorsx.G11NError("unable to get the Identitysources")
+	}
+
+	return IdentitysourcesResponse, resp.HTTPResponse.Request.URL.String(), nil
+}
+
+func (c *IdentitysourceClient) DeleteIdentitysourceByName(ctx context.Context, name string) error {
+	vc := contextx.GetVerifyContext(ctx)
+	client := openapi.NewClientWithOptions(ctx, vc.Tenant, c.Client)
+	id, err := c.getIdentitysourceId(ctx, name)
+	if err != nil {
+		vc.Logger.Errorf("unable to get the identitysource ID; err=%s", err.Error())
+		return errorsx.G11NError("unable to get the identitysource ID; err=%s", err.Error())
+	}
+
+	resp, err := client.DeleteIdentitySourceV2WithResponse(ctx, id, func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", vc.Token))
+		return nil
+	})
+	if err != nil {
+		vc.Logger.Errorf("unable to delete the IdentitySource; err=%s", err.Error())
+		return errorsx.G11NError("unable to delete the IdentitySource; err=%s", err.Error())
+	}
+
+	if resp.StatusCode() != http.StatusNoContent {
+		if err := errorsx.HandleCommonErrors(ctx, resp.HTTPResponse, "unable to delete IdentitySource"); err != nil {
+			vc.Logger.Errorf("unable to delete the IdentitySource; err=%s", err.Error())
+			return errorsx.G11NError("unable to delete the IdentitySource; err=%s", err.Error())
+		}
+
+		vc.Logger.Errorf("unable to delete the IdentitySource; code=%d, body=%s", resp.StatusCode(), string(resp.Body))
+		return errorsx.G11NError("unable to delete the IdentitySource; code=%d, body=%s", resp.StatusCode(), string(resp.Body))
+	}
+
+	return nil
+}
+
+func (c *IdentitysourceClient) UpdateIdentitysource(ctx context.Context, identitysource *IdentitySource) error {
+	vc := contextx.GetVerifyContext(ctx)
+	client := openapi.NewClientWithOptions(ctx, vc.Tenant, c.Client)
+	defaultErr := errorsx.G11NError("unable to update identitysource")
+	id, err := c.getIdentitysourceId(ctx, identitysource.InstanceName)
+	if err != nil {
+		vc.Logger.Errorf("unable to get the identitysource ID; err=%s", err.Error())
+		return errorsx.G11NError("unable to get the identitysource ID; err=%s", err.Error())
+	}
+	body, err := json.Marshal(identitysource)
+	if err != nil {
+		vc.Logger.Errorf("Unable to marshal identitysource data; err=%v", err)
+		return defaultErr
+	}
+
+	resp, err := client.UpdateIdentitySourceV2WithBodyWithResponse(ctx, id, "application/json", bytes.NewBuffer(body), func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", vc.Token))
+		return nil
+	})
+
+	if err != nil {
+		vc.Logger.Errorf("unable to update identitysource; err=%v", err)
+		return errorsx.G11NError("unable to update identitysource; err=%v", err)
+	}
+
+	if resp.StatusCode() != http.StatusNoContent {
+		vc.Logger.Errorf("failed to update identitysource; code=%d, body=%s", resp.StatusCode(), string(resp.Body))
+		return errorsx.G11NError("failed to update identitysource ; code=%d, body=%s", resp.StatusCode(), string(resp.Body))
+	}
+
+	return nil
+}
+
+func (c *IdentitysourceClient) getIdentitysourceId(ctx context.Context, name string) (string, error) {
+	vc := contextx.GetVerifyContext(ctx)
+	client := openapi.NewClientWithOptions(ctx, vc.Tenant, c.Client)
+	search := fmt.Sprintf(`instanceName = "%s"`, name)
+	params := &openapi.GetInstancesV2Params{
+		Search: &search,
+	}
+	resp, _ := client.GetInstancesV2WithResponse(ctx, params, func(ctx context.Context, req *http.Request) error {
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", vc.Token))
+		return nil
+	})
+
+	if resp.StatusCode() != http.StatusOK {
+		if err := errorsx.HandleCommonErrors(ctx, resp.HTTPResponse, "unable to get IdentitySource"); err != nil {
+			vc.Logger.Errorf("unable to get the IdentitySource with identitysourceName %s; err=%s", name, err.Error())
+			return "", errorsx.G11NError("unable to get the IdentitySource with identitysourceName %s; err=%s", name, err.Error())
+		}
+	}
+
+	var data map[string]interface{}
+	if err := json.Unmarshal(resp.Body, &data); err != nil {
+		return "", errorsx.G11NError("failed to parse response: %w", err)
+	}
+
+	resources, ok := data["identitySources"].([]interface{})
+	if !ok || len(resources) == 0 {
+		return "", errorsx.G11NError("no identitysource found with identitysourceName %s", name)
+	}
+
+	firstResource, ok := resources[0].(map[string]interface{})
+	if !ok {
+		return "", errorsx.G11NError("invalid resource format")
+	}
+
+	// Extract "id" field
+	id, ok := firstResource["id"].(string)
+	if !ok {
+		return "", errorsx.G11NError("ID not found or invalid type")
+	}
+
+	return id, nil
+}
